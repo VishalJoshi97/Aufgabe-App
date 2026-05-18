8602497ca4f4000532a2bccf484a75377e22b2cf
VishalJoshi97 <dharmendrakulkarni962@gmail.com>
2026-05-18 22:29:44 +0530

    wrap with BottomSheetModalProvider

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,16 +1,17 @@
-import "react-native-gesture-handler";
-
 import { Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import { AuthProvider } from "@/src/context/AuthContext";
+import {BottomSheetModalProvider} from "@gorhom/bottom-sheet";
 
 export default function RootLayout() {
     return (
         <GestureHandlerRootView style={{ flex: 1 }}>
+            <BottomSheetModalProvider>
             <AuthProvider>
                 <Stack screenOptions={{ headerShown: false }} />
             </AuthProvider>
+            </BottomSheetModalProvider>
         </GestureHandlerRootView>
     );
 }
